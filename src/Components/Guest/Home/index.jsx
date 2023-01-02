8b7d7079571b0ee0639efe532471579ebc66beca
Edrie Isaac Cruz <eddsycruz@gmail.com>
2023-01-02 20:43:19 +0800

    added date happened

--- src/Components/Guest/Home/index.jsx
+++ src/Components/Guest/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import './index.scss'
 
 // Icons
@@ -7,15 +7,14 @@ import { faInstagram, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-
 
 const Home = () => {
 
-  
-
   return (
     <div className='section home'>
       <div className='home-img'>
         <img 
           src="https://firebasestorage.googleapis.com/v0/b/krstn-f3236.appspot.com/o/Home%26Info%2FIMG_HOME.jpg?alt=media&token=65ac3f35-e14c-4e82-bc93-d77ba8786265" 
           alt='Home-Img' 
-          className='this-home-img'/>
+          className='this-home-img'
+          loading='lazy'/>
         
       </div>
 
